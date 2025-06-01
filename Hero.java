138b0f1e4d53ab6d87c03b1fbb5a119df460b8a0
hurricanzy <savateev.da@dvfu.ru>
2025-06-01 19:09:30 +1000

    update hero and add platform

--- Hero.java
+++ Hero.java
@@ -1,102 +1,126 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Hero extends Actor{
-    
-    
-    /* Для анимашки **/
-    GreenfootImage StaticHero;
-    GreenfootImage StaticHeroHorizont;
-    GreenfootImage[] walkRight = new GreenfootImage[3];
-    GreenfootImage[] walkLeft = new GreenfootImage[3];
-    boolean facingRight = true;  /* True вправо/left влево  **/
-    int HereWeGo = 1;
-    int animationDelay = 5;
-    int animationCounter = 0;
-    
-    /* Для игровой механики платоформера **/
-    int speedH = 5;
+public class Hero extends Actor 
+{
+    //анимации
+    GreenfootImage StaticHero; 
+    GreenfootImage StaticHeroHorizont; 
+    GreenfootImage[] walkRight = new GreenfootImage[3]; 
+    GreenfootImage[] walkLeft = new GreenfootImage[3]; 
+    boolean facingRight = true;  
+    int HereWeGo = 1; 
+    int animationDelay = 5; 
+    int animationCounter = 0; 
+
+    //механика движения
+    int speedH = 5; 
     int gravity = 1;
-    int speedV = 0;
+    int speedV = 0; 
     final int Jump = -15;
 
     
-    public Hero (){
-        
-        for(int i = 0; i < 3; i++) {
+    public Hero() 
+    {
+        //анимации ходьбы
+        for (int i = 0; i < 3; i++) 
+        {
             walkRight[i] = new GreenfootImage("spriteWalk" + i + ".png");
             walkLeft[i] = new GreenfootImage("spriteWalk" + i + ".png");
-            walkLeft[i].mirrorHorizontally();
-            walkRight[i].scale(75,75);
-            walkLeft[i].scale(75,75);
+            walkLeft[i].mirrorHorizontally(); 
+            walkRight[i].scale(75, 75);
+            walkLeft[i].scale(75, 75);
         }
-        
+
+        //статичное изобр
         StaticHero = new GreenfootImage("staticHero.png");
-        StaticHero.scale(75,75);
+        StaticHero.scale(75, 75);
         StaticHeroHorizont = new GreenfootImage(StaticHero);
         StaticHeroHorizont.mirrorHorizontally();
-        setImage(StaticHero);
-    
+        setImage(StaticHero); 
     }
-    
-    public void act(){
-        moveV();
-        animate();
-        fall();
-        checkLand();
+
+    public void act()
+    {
+        moveV();  //обработка управления
+        animate(); //анимация
+        fall();  //применение гравитации
+        checkLand(); //проверка на приземление
     }
+
     
-    public void moveV(){
-        
-        if (Greenfoot.isKeyDown("space") && check()) {
-            speedV = Jump;}
-            
-        if ((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a")) && getX() > getImage().getWidth()/ 2 - 15){
+    public void moveV() 
+    {
+        if (Greenfoot.isKeyDown("space") && check()) 
+        {
+            speedV = Jump;
+        }
+
+        if ((Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a")) && getX() > getImage().getWidth() / 2 - 15) 
+        {
             setLocation(getX() - speedH, getY());
             HereWeGo = 0;
             facingRight = false;
-        }else if ((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d")) && (getX() < getWorld().getWidth() - getImage().getWidth()/2)){
+        }
+        else if ((Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d")) && getX() < getWorld().getWidth() - getImage().getWidth() / 2) 
+        {
             setLocation(getX() + speedH, getY());
             HereWeGo = 2;
             facingRight = true;
-        }else{
+        }
+        else 
+        {
             HereWeGo = 1;
         }
-    } 
-    
-    public void animate(){
+    }
+
+    public void animate() 
+    {
         ++animationCounter;
         int frame = (animationCounter / animationDelay) % 3;
-        if (HereWeGo == 2){
+
+        if (HereWeGo == 2) 
+        { 
             setImage(walkRight[frame]);
-        }else if(HereWeGo == 0){
+        } else if (HereWeGo == 0) 
+        { 
             setImage(walkLeft[frame]);
-        }else{
-            if(facingRight)setImage(StaticHero);
-            else setImage(StaticHeroHorizont);
-            
+        } else 
+        {
+            if (facingRight)
+                setImage(StaticHero);
+            else
+                setImage(StaticHeroHorizont);
         }
-    
     }
-    public boolean check(){
-        if (speedV == 0)return true;
-        return false;
+
+    public boolean check() 
+    {
+        return isTouching(Platform.class) || getY() >= 310;
     }
-    public void checkLand() {
-        if (getY() >= 310  && speedV > 0) {
+
+    public void checkLand() 
+    {
+        Actor platform = getOneObjectAtOffset(0, getImage().getHeight() / 2 + 5, Platform.class);
+
+        //приземление на платформу
+        if (platform != null && speedV >= 0) 
+        {
+            int platformTop = platform.getY() - platform.getImage().getHeight() / 2;
+            int heroHalfHeight = getImage().getHeight() / 2;
+            setLocation(getX(), platformTop - heroHalfHeight);
+            speedV = 0;
+        } 
+        //приземление на землю
+        else if (getY() >= 310 && speedV >= 0) 
+        {
             setLocation(getX(), 310);
             speedV = 0;
         }
     }
-    
-    public void fall(){
-            speedV += gravity;
-            setLocation(getX(), getY() + speedV);
+
+    public void fall() 
+    {
+        speedV += gravity; 
+        setLocation(getX(), getY() + speedV); 
     }
 }
-
---
+++ Platform.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+
+public class Platform extends Actor 
+{
+    public Platform() 
+    {
+        GreenfootImage img = new GreenfootImage("platform.png");
+        img.scale(100, 20); 
+        setImage(img);
+    }
+}
